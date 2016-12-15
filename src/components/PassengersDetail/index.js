ebdbace82ae5bbe90856f50e857a9e370cf045c4
Ефимов Сергей Александрович <sefimov@at-consulting.ru>
2016-12-15 18:25:08 +0300

    continues work with passengers forms

--- src/components/PassengersDetail/index.js
+++ src/components/PassengersDetail/index.js
@@ -1,7 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import moment from 'moment';
 
 // import library from "../../lib/library";
+
+import passengersTypes from '../../data/passengersTypes';
 
 class TravelerInformation extends Component {
 	render() {
@@ -33,13 +35,14 @@ class PassengersDetail extends Component {
 		this.state = {
 			chosenFlight: {},
 			customOffers: [],
+			passengers: {},
 		};
 	}
 
 	componentDidMount() {
 		let passengers = {};
 
-		{this.props.dataLists.anonymousTravelerList.map(type => {
+		this.props.dataLists.anonymousTravelerList.map(type => {
 			passengers[type.ptc.value] = [];
 
 			for (let count = 1; count <= type.ptc.quantity; count++) {
@@ -65,62 +68,107 @@ class PassengersDetail extends Component {
 					}
 				})
 			}
-		})}
+		})
+
+		this.setState({ passengers });
+
 	}
 
 	render() {
 		const { dataLists, searchParams } = this.props;
+		const { passengers } = this.state;
 
 		return (
 			<div>
-				<div className="col-xs-4">
-					<h4>Пассажиры</h4>
-					<ul>
-						{dataLists.anonymousTravelerList.map((type, id) => {
-							let title = '';
+				<div className="row">
+					<div className="col-xs-4">
+						<h4>Пассажиры</h4>
+						<ul>
+							{dataLists.anonymousTravelerList.map((type, id) => {
+								let title = '';
 
-							if (type.ptc.value === 'ADT') {
-								title = 'Взрослых: '
-							}
+								if (type.ptc.value === 'ADT') {
+									title = 'Взрослых: '
+								}
 
-							if (type.ptc.value === 'CNN') {
-								title = 'Детей: '
-							}
+								if (type.ptc.value === 'CNN') {
+									title = 'Детей: '
+								}
 
-							if (type.ptc.value === 'INF') {
-								title = 'Младенцев: '
-							}
+								if (type.ptc.value === 'INF') {
+									title = 'Младенцев: '
+								}
 
-							return (
-								<li key={id}>{title} {type.ptc.quantity}</li>
-							)
-						})}
-					</ul>
+								return (
+									<li key={id}>{title} {type.ptc.quantity}</li>
+								)
+							})}
+						</ul>
+					</div>
 				</div>
 
-				<h2>Информация о пассажирах</h2>
+				<div className="row">
+					<div className="col-xs-12">
+						<h2>Информация о пассажирах</h2>
 
-				<div id="passenger-notification" className="alertFullWidth">
-					Фамилия, имя, отчество и номер документа пассажира вводятся латинскими буквами. При
-					внесении данных заграничного паспорта - как указано в заграничном паспорте. При внесении
-					данных паспорта гражданина РФ в поле «Срок действия» вводится любая дата более поздняя,
-					чем дата Вашего полета. Номер документа вводится без пробелов.
+						<div id="passenger-notification" className="alertFullWidth">
+							Фамилия, имя, отчество и номер документа пассажира вводятся латинскими буквами. При
+							внесении данных заграничного паспорта - как указано в заграничном паспорте. При
+							внесении
+							данных паспорта гражданина РФ в поле «Срок действия» вводится любая дата более
+							поздняя,
+							чем дата Вашего полета. Номер документа вводится без пробелов.
 
-					<p className="singleLine">
-						Информация о документе пассажира вносится по требованию Министерства транспорта РФ.
-					</p>
+							<p className="singleLine">
+								Информация о документе пассажира вносится по требованию Министерства транспорта РФ.
+							</p>
 
-					<a href="https://www.aeroflot.ru/cms/before_and_after_fly/rules_online?_ga=1.57626666.
+							<a href="https://www.aeroflot.ru/cms/before_and_after_fly/rules_online?_ga=1.57626666.
 					1656093309.1481571411"
-					   target="_blank">
-						Пассажир несет ответственность за корректность ввода паспортных данных.
-					</a>
+							   target="_blank">
+								Пассажир несет ответственность за корректность ввода паспортных данных.
+							</a>
+						</div>
+
+						<h4>
+							Пассажирам c одинаковыми именами и фамилиями требуется указать суффикс.<br />
+							Внимание ! Взрослый пассажир, сопровождающий ребенка, не может быть моложе 18 лет.
+						</h4>
+
+						<small className="field-required-legend">
+							Все поля, отмеченные * , являются обязательными для заполнения
+						</small>
+					</div>
 				</div>
 
-				<TravelerInformation />
-				<TravelerPreferences />
-				<TravelerDocument />
+				{passengersTypes.forEach((type, id) => {
+					console.log('passengersTypes', passengersTypes);
+					console.log('passengers', passengers);
+					console.log('type.id', type.id);
+					console.log('passengers[type.id]', passengers[type.id]);
 
+					if (passengers[type.id]) {
+						return (
+							<div>
+								<h4>{type.text}</h4>
+
+								{passengers[type.id].map((passenger, passid) => {
+									console.log('alarm', passenger);
+
+									return (
+										<div key={passid}>
+											{type.id};
+											<TravelerInformation />
+											<TravelerPreferences />
+											<TravelerDocument />
+										</div>
+									)
+								})}
+							</div>
+						)
+					}
+
+				})}
 			</div>
 		)
 	}
---
+++ src/data/passengersTypes.js
@@ -0,0 +1,16 @@
+const passengersTypes = [
+	{
+		id: 'ADT',
+		text: 'Взрослые'
+	},
+	{
+		id: 'CNN',
+		text: 'Дети'
+	},
+	{
+		id: 'INF',
+		text: 'Младенцы'
+	}
+];
+
+export default passengersTypes;
